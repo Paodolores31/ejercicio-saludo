7c213bd1d57076a17b7b103b288eeef85ac2c17c
paodolores31 <pdolores471@gmail.com>
2024-10-28 10:42:23 -0600

    primer commit

---
+++ nombre.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   string nombre;
+   
+   cout<<"Ingrese un nombre ";
+   cin>>nombre;
+   
+   cout<<"Hola "<<nombre<<endl;
+   
+
+    return 0;
+}
